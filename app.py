fa839e89ee8ddeb7c99271e09b0ff5a735d45989
SikioN <nmuravya@mail.ru>
2026-07-04 19:53:29 +0300

    fix: bind uvicorn port in app.py to PORT environment variable

--- app.py
+++ app.py
@@ -515,4 +515,5 @@ if os.path.exists(webui_assets_dir):
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run("app:app", host="0.0.0.0", port=port, reload=True)
